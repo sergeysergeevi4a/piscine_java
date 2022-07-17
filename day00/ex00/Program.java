ad25902879e3b07161d37b4a7a1a791aadd544f0
Katina Ferterbest <kferterb@pr-o2.kzn.21-school.ru>
2022-07-17 14:43:20 +0300

    java

--- day00/ex00/Program.java
+++ day00/ex00/Program.java
@@ -6,7 +6,34 @@
 /*   By: kferterb <kferterb@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 10:15:39 by kferterb          #+#    #+#             */
-/*   Updated: 2022/07/17 10:15:40 by kferterb         ###   ########.fr       */
+/*   Updated: 2022/07/17 14:42:06 by kferterb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+public class Program {
+
+	public static void main(String[] args)
+	{
+		int num = 479598;
+		int res = 0;
+		
+		res = res + num % 10;
+		num = num / 10;
+
+		res = res + num % 10;
+		num = num / 10;
+
+		res = res + num % 10;
+		num = num / 10;
+
+		res = res + num % 10;
+		num = num / 10;
+		
+		res = res + num % 10;
+		num = num / 10;
+		
+		res = res + num % 10;
+		
+		System.out.println(res);
+	}
+}
