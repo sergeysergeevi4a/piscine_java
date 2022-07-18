544b3ac233887de0146f40d33a0bd6fabca025d3
intituta <intituta@tutanota.com>
2022-07-18 09:56:45 +0300

    java

---
+++ day00/ex03/Program.java
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Program.java                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kferterb <kferterb@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/17 19:00:43 by kferterb          #+#    #+#             */
+/*   Updated: 2022/07/18 09:56:23 by kferterb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+import java.util.Scanner;
+
+class Program {
+	
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		
+		while (scanner.hasNext()) {
+
+		
+			
+		}
+		
+		scanner.close();
+	}
+}
