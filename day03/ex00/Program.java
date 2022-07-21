6ae0eef5fe9a43ddaa0b37b01631941c3302c30f
intituta <intituta@tutanota.com>
2022-07-21 10:36:04 +0300

    java

--- day03/ex00/Program.java
+++ day03/ex00/Program.java
@@ -6,13 +6,12 @@
 /*   By: kferterb <kferterb@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 10:18:48 by kferterb          #+#    #+#             */
-/*   Updated: 2022/07/21 10:33:19 by kferterb         ###   ########.fr       */
+/*   Updated: 2022/07/21 10:36:00 by kferterb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 class Main {
 	public static void main(String[] args) {
-		System.out.println("hi");
-		System.out.println("hi");
+		
 	}
 }
