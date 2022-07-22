69bb6c04e465d71e3c999e19f2faabd1743c7d10
intituta <intituta@tutanota.com>
2022-07-22 15:53:58 +0300

    java

--- day04/ex00/ImagesToChar/src/java/edu/school21/printer/app/Program.java
+++ day04/ex00/ImagesToChar/src/java/edu/school21/printer/app/Program.java
@@ -6,17 +6,35 @@
 /*   By: kferterb <kferterb@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 09:58:06 by kferterb          #+#    #+#             */
-/*   Updated: 2022/07/22 13:29:53 by kferterb         ###   ########.fr       */
+/*   Updated: 2022/07/22 15:52:28 by kferterb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 package edu.school21.printer.app;
 
+import java.io.IOException;
 import edu.school21.printer.logic.Logic;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		if (args.length != 3) {
+			System.out.println("Wrong arguments");
+			System.exit(-1);
+		}
+		
+		char whiteColor = args[0].charAt(0);
+		char blackColor = args[1].charAt(0);
+		String path = args[2];
+
+		int[][] mass = Logic.seeBMPImage(path, blackColor, whiteColor);
+		
+		for (int x = 0; x < mass.length; x++) {
+			for (int y = 0; y < mass[x].length; y++) {
+				System.out.print((char)mass[y][x]);
+			}
+			System.out.println();
+		}
 	}
 }
--- day04/ex00/ImagesToChar/src/java/edu/school21/printer/logic/Logic.java
+++ day04/ex00/ImagesToChar/src/java/edu/school21/printer/logic/Logic.java
@@ -6,12 +6,37 @@
 /*   By: kferterb <kferterb@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 09:58:22 by kferterb          #+#    #+#             */
-/*   Updated: 2022/07/22 13:30:00 by kferterb         ###   ########.fr       */
+/*   Updated: 2022/07/22 15:50:59 by kferterb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 package edu.school21.printer.logic;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 public class Logic {
 	
+	public static int index = 10;
+
+	public static int[][] seeBMPImage(String path, char blackColor, char whiteColor) throws IOException {
+		BufferedImage image = ImageIO.read(new FileInputStream(path));
+
+		int mass[][] = new int[image.getWidth()][image.getHeight()];
+		
+		for (int xPixel = 0; xPixel < image.getWidth(); xPixel++) {
+			for (int yPixel = 0; yPixel < image.getHeight(); yPixel++) {
+				int color = image.getRGB(xPixel, yPixel);
+				if (color == Color.BLACK.getRGB()) {
+					mass[xPixel][yPixel] = blackColor;
+				} else if (color == Color.WHITE.getRGB()){
+					mass[xPixel][yPixel] = whiteColor;
+				}
+			}
+		}
+		return mass;
+	}
 }
